09fe6e4872db828a0914ba06295e4fea4c8b0cda
Martin Riedel <mrtn@mrtnDesktop.Speedport_W723_V_Typ_A_1_01_016>
2016-04-23 11:34:41 +0200

    Modularized zsh configurations.

--- zsh/.zsh/environment.zsh
+++ zsh/.zsh/environment.zsh
@@ -1,3 +1,21 @@
+# Path to your oh-my-zsh installation.
+export ZSH=/home/mrtn/.oh-my-zsh
+
 # theme
 ZSH_THEME="robbyrussell"
 
+# plugins
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(git)
+
+# language
+export LC_COLLATE=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_MESSAGES=en_US.UTF-8
+export LC_MONETARY=en_US.UTF-8
+export LC_NUMERIC=en_US.UTF-8
+export LC_TIME=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LESSCHARSET=utf-8
--- zsh/.zsh/user.zsh
+++ zsh/.zsh/user.zsh
@@ -1,2 +1,4 @@
 # User configuration
 export PATH="/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/home/mrtn/.local/bin:/home/mrtn/bin"
+source $ZSH/oh-my-zsh.sh
+
