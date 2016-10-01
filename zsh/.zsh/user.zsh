c16745f58bb3347f6f1b351b8d09c72e191fdb3b
Martin Riedel <mail@mrtnrdl.de>
2016-10-01 17:33:35 +0200

    Added export java home.

--- zsh/.zsh/user.zsh
+++ zsh/.zsh/user.zsh
@@ -1,4 +1,8 @@
 # User configuration
 export PATH="/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/home/mrtn/.local/bin:/home/mrtn/bin"
+
+#java home
+export JAVA_HOME="/usr/java/latest"
+
 source $ZSH/oh-my-zsh.sh
 
