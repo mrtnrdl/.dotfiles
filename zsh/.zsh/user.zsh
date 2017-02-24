63503366a28be621a5a3e7b08ad7c972cc00eecf
Martin Riedel <mail@mrtnrdl.de>
2017-02-24 15:56:51 +0100

    Updated i3bar and removed path override in zsh config.

--- zsh/.zsh/user.zsh
+++ zsh/.zsh/user.zsh
@@ -1,8 +1,5 @@
 # User configuration
 export PATH="/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/home/mrtn/.local/bin:/home/mrtn/bin:/usr/bin/core_perl"
 
-#java home
-export JAVA_HOME="/usr/java/latest"
-
 source $ZSH/oh-my-zsh.sh
 
