334cf3857eef8475d1fefee9fcb8cf5e78094bb5
Martin Riedel <mail@mrtnrdl.de>
2017-10-27 08:35:40 +0200

    Moved path to machine-specific.zsh

--- zsh/.zsh/user.zsh
+++
@@ -1,5 +0,0 @@
-# User configuration
-export PATH="/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/home/mrtn/.local/bin:/home/mrtn/bin:/usr/bin/core_perl"
-
-source $ZSH/oh-my-zsh.sh
-
